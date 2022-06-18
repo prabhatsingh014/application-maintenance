91f14b1944dff45f5ae53653fe75ea4e62377e61
Prabhat Singh <prabhatsingh014@gmail.com>
2022-06-18 21:15:36 +0530

    updated healthcheck.sh

--- scripts/healthcheck.sh
+++ scripts/healthcheck.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x
 echo ${PACKAGE}
-ansible-playbook $CI_PROJECT_DIR/playbooks/healthcheck.yml -e 'package=${PACKAGE}' -i $CI_PROJECT_DIR/inventory -vv
+ansible-playbook $CI_PROJECT_DIR/playbooks/healthcheck.yml -e 'package=$"{PACKAGE}"' -i $CI_PROJECT_DIR/inventory -vv
